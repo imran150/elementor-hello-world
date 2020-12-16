2a4516231a0159e28861ac224cff7b6f8b26ff98
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2020-12-16 23:28:43 +0600

    Added widget files

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+class Elementor_Test_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {}
+
+	public function get_title() {}
+
+	public function get_icon() {}
+
+	public function get_categories() {}
+
+	protected function _register_controls() {}
+
+	protected function render() {}
+
+	protected function _content_template() {}
+
+}
